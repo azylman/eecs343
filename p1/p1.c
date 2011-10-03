8cb1c2196668b3479dbca4e5a20289c451d6acfd
Alex Zylman <alex@zylman.com>
2011-10-03 16:36:24 -0500

    added simple hello world with command line arguments

--- p1/p1.c
+++ p1/p1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf( "I am alive!  Beware.\n" );
-	getchar();
+	printf("Hello world!\n");
+	printf("There are %u arguments", argc - 1);
+	if (argc > 1) {
+		printf(", the first of which is %s", argv[1]);
+	}
+	printf(".\n");
 	return 0;
 }
